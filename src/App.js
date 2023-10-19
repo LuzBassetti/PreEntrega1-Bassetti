dfc278770755e286b5aee891d357ec77368071e5
Luz Bassetti <luzbassetti95@gmail.com>
2023-10-19 17:44:15 -0300

    agregado de package.json

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting='Bienvendidos a Pasteleria y Decoracion'/>
+      <ItemListContainer greeting='Bienvendidos a Pasteleria y Decoración'/>
       <Footer />
     </div>
   );
